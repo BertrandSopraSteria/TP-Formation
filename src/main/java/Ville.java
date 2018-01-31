4da7e3c1693cc341a8d5993b3a009493bfecaa87
Bertrand <bertrand.pham@soprasteria.com>
2018-01-31 15:34:39 +0100

    modification structure de la ville

--- src/main/java/Ville.java
+++ src/main/java/Ville.java
@@ -1,7 +1,9 @@
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Version;
 
+@Entity
 public class Ville {
 
 	@Id
@@ -14,20 +16,25 @@ public class Ville {
 	public String getCode() {
 		return code;
 	}
+	
 	public void setCode(String code) {
 		this.code = code;
 	}
+	
 	public String getLibelle() {
 		return libelle;
 	}
+	
 	public void setLibelle(String libelle) {
 		this.libelle = libelle;
 	}
+	
 	public Ville(String code, String libelle) {
 		super();
 		this.code = code;
 		this.libelle = libelle;
 	}
+	
 	public Ville() {
 		super();
 		// TODO Auto-generated constructor stub
