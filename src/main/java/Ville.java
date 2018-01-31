362b0ab581118e14f0cf18f3a03dabbca8a08217
Bertrand <bertrand.pham@soprasteria.com>
2018-01-31 15:18:28 +0100

    ajout de la classe ville

---
+++ src/main/java/Ville.java
@@ -0,0 +1,39 @@
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+public class Ville {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	@Version
+	private int version;
+	private String code;
+	private String libelle;
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getLibelle() {
+		return libelle;
+	}
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	public Ville(String code, String libelle) {
+		super();
+		this.code = code;
+		this.libelle = libelle;
+	}
+	public Ville() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+}
